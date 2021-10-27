7762ef5d5130767a5a6f927db495a6d20f83f975
Piotr Ciepły <cieply1101@gmail.com>
2021-10-27 23:07:02 +0200

    init project

---
+++ main.py
@@ -0,0 +1,12 @@
+shopping_list={
+    "warzywniak":["marchew", "ogórek", "sałata"],
+    "zoologiczny":["jedzenie dla kota"],
+    "piekarnia":["chleb", "ciasto"],
+    "mięsny":["szynka", "kurczak"]
+    }
+product_list=[]
+for store in shopping_list:
+    products=str(shopping_list[store])
+    print(f"Idę do {store.upper()} i kupuję tam {products.upper()}")
+    product_list+=shopping_list[store]
+print(f"W sumię kupiłem {len(product_list)} produktów.")
